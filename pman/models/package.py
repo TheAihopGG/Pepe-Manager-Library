5e50637316775c467d44fad13c7f90662a90948d
TheAihopGG <theaihopgg@gmail.com>
2025-03-31 22:30:31 +0700

    fix: type conversion: version, data. And `dir_name`

--- pman/models/package.py
+++ pman/models/package.py
@@ -1,13 +1,34 @@
-from dataclasses import dataclass
+import versions
 
 
-@dataclass
 class Package:
-    id: int
-    name: str
-    description: str
-    version: str
-    author_name: str
-    data: str
-    created_at: int
-    updated_at: int
+    def __init__(
+        self,
+        id: int,
+        name: str,
+        description: str,
+        version: str | versions.Version,
+        data: bytes | str,
+        author_name: str,
+        created_at: int,
+        updated_at: int,
+    ):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.author_name = author_name
+        self.created_at = created_at
+        self.updated_at = updated_at
+        self.version: versions.Version
+        self.data: bytes
+
+        if isinstance(version, str):
+            self.version = versions.parse_version(version)
+        else:
+            self.version = version
+        if isinstance(data, str):
+            self.data = bytes(data, "utf-8")
+        else:
+            self.data = data
+
+        self.dir_name = self.name + self.version.to_string()
--- pman/models/package_info.py
+++ pman/models/package_info.py
@@ -1,12 +1,24 @@
-from dataclasses import dataclass
+import versions
 
 
-@dataclass
 class PackageInfo:
-    id: int
-    name: str
-    description: str
-    version: str
-    author_name: str
-    created_at: int
-    updated_at: int
+    def __init__(
+        self,
+        id: int,
+        name: str,
+        description: str,
+        version: str | versions.Version,
+        author_name: str,
+        created_at: int,
+        updated_at: int,
+    ):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.author_name = author_name
+        self.created_at = created_at
+        self.updated_at = updated_at
+        if isinstance(version, str):
+            self.version = versions.parse_version(version)
+        else:
+            self.version = version
