a44f0a70c1e474d2fd96a2d6d773114f670fa5ca
TheAihopGG <theaihopgg@gmail.com>
2025-03-31 22:29:35 +0700

    feat: initial

---
+++ pman/urls.py
@@ -0,0 +1,7 @@
+from typing import ClassVar
+
+
+class Urls:
+    GET_PACKAGE: ClassVar[str] = "/public/api/packages/package"
+    GET_PACKAGE_INFO: ClassVar[str] = "/public/api/packages/package_info"
+    GET_PACKAGES_INFO: ClassVar[str] = "/public/api/packages/packages_info"
